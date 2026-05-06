488d61ad7bc93363be4300ae91c33fa2a336dd6d
Esteban <etirini18@hotmail.com>
2026-05-05 21:01:48 -0400

    10

---
+++ 10_Day_Loops/D10 Solutions.py
@@ -0,0 +1,36 @@
+#1
+"""
+for number in range(11):
+    print(number)
+
+num = 0
+while num <= 10:
+    print(num)
+    num +=1
+
+#2
+
+for number in range(10,-1,-1):
+    print(number)
+
+num = 10
+while num >= 0:
+    print(num)
+    num -=1
+
+#3
+for row in range(7):
+    print('#' * row)
+
+
+#4
+for i in range(8):
+    for j in range(8):
+        print('# ')
+    print()
+
+"""
+
+#5
+for j in range(11):
+    print(f"{j} * {j} = {j*j}", end='\n')
